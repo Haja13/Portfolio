1d18360a422fd83a81b439524f0c337fcb96a962
haja <randev140@randev.ovh>
2023-08-01 09:00:36 +0200

    modification app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import NavBarComponent from './component/NavBar/NavBar';
 import './style.css'
 import SpecialityComponent from './component/Speciality/specialityComponent';
 import Education from './component/Education/Education';
-import {Routes,Route} from 'react-router-dom'
-import Blog from './blog';
-import About from './about';
-import Accueil from './accueil';
-import Portfolio from './portfolio';
 import PersonalComponent from './component/PersonalComponent/PersonalComponent';
 import Contact from './component/contact/contact';
 const App = () => {
